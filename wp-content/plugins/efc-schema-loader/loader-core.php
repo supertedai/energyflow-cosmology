18c13fcc267c45c6b8ef0684daf65f74885f05a7
Morten Magnusson <morten@magnusson.as>
2025-11-14 11:30:34 +0100

    Create loader-core.php

---
+++ wp-content/plugins/efc-schema-loader/loader-core.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Core loader for EFC Schema Loader plugin
+ */
+
+// Your existing efc_all_schema_sources(), efc_fetch_remote_schema(), efc_output_full_schema_bundle()
+// should be moved here from theme functions.php soon
