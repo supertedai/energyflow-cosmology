297e62161438bf6ee0fbc2f331fc2341c3717626
Morten Magnusson <morten@magnusson.as>
2025-11-22 19:11:15 +0100

    Update config.py

--- symbiose_gnn/config.py
+++ symbiose_gnn/config.py
@@ -1,6 +1,10 @@
+# symbiose_gnn/config.py
+
 config = {
-    "hidden_dim": 64,
-    "epochs": 50,
-    "lr": 0.01,
+    "neo4j_uri": "neo4j+s://119e751c.databases.neo4j.io",
+    "neo4j_user": "neo4j",
+    "neo4j_password": "N-HPl8pKFVwsMgCzydGI26dsgJAMOP1ss6r1NhiHNjs",
+    "neo4j_database": "neo4j",
     "output_dir": "symbiose_gnn_output",
+    "embedding_dim": 64
 }
