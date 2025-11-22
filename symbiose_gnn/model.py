d47ff740750709392f0fe065a08bb5bfc2097875
Morten Magnusson <morten@magnusson.as>
2025-11-22 19:11:43 +0100

    Update model.py

--- symbiose_gnn/model.py
+++ symbiose_gnn/model.py
@@ -1,33 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import torch
 import torch.nn as nn
-from torch_geometric.nn import SAGEConv
+from torch_geometric.nn import GCNConv
+from .config import config
 
 
 class SymbioseGNN(nn.Module):
-    def __init__(self, in_dim: int, hidden_dim: int, out_dim: int, num_layers: int = 2):
+    def __init__(self, input_dim=config["embedding_dim"], hidden_dim=128, out_dim=64):
         super().__init__()
-        layers = []
+        self.conv1 = GCNConv(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.conv2 = GCNConv(hidden_dim, out_dim)
 
-        # Første lag
-        layers.append(SAGEConv(in_channels=in_dim, out_channels=hidden_dim))
-
-        # Mellomlag
-        for _ in range(num_layers - 2):
-            layers.append(SAGEConv(in_channels=hidden_dim, out_channels=hidden_dim))
-
-        # Siste lag
-        if num_layers > 1:
-            layers.append(SAGEConv(in_channels=hidden_dim, out_channels=out_dim))
-
-        self.layers = nn.ModuleList(layers)
-        self.activation = nn.ReLU()
-
-    def forward(self, x, edge_index):
-        for i, layer in enumerate(self.layers):
-            x = layer(x, edge_index)
-            if i < len(self.layers) - 1:
-                x = self.activation(x)
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = self.relu(x)
+        x = self.conv2(x, edge_index)
         return x
