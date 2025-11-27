708d332511fffb9fc0818cf6674f0019ba188c6f
Morten Magnusson <morten@magnusson.as>
2025-11-27 20:14:23 +0100

    Update neo4j.py

--- apis/unified-api/routers/neo4j.py
+++ apis/unified-api/routers/neo4j.py
@@ -1,1 +1,9 @@
+from fastapi import APIRouter
+from clients.neo4j_client import run_query
+
+router = APIRouter(prefix="/neo4j")
+
+@router.get("/q")
+def q(query: str):
+    return run_query(query)
 
