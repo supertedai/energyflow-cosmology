d5b61e9bfbb8ab59219463e56871dc97e9c32252
Morten Magnusson <morten@magnusson.as>
2025-11-27 20:15:04 +0100

    Update neo4j_client.py

--- apis/unified-api/clients/neo4j_client.py
+++ apis/unified-api/clients/neo4j_client.py
@@ -1,1 +1,11 @@
+import os
+from neo4j import GraphDatabase
 
+driver = GraphDatabase.driver(
+    os.environ["NEO4J_URI"],
+    auth=(os.environ["NEO4J_USER"], os.environ["NEO4J_PASSWORD"])
+)
+
+def run_query(q: str):
+    with driver.session() as s:
+        return s.run(q).data()
