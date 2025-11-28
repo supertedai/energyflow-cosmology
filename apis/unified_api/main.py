9cc9742f48339a465ae67f8b689ff540fadd386a
Morten Magnusson <morten@magnusson.as>
2025-11-28 11:13:08 +0000

    Lag 1a stabil – unified-api fixed, ingest/embed disabled, Neo4j OK

--- apis/unified_api/main.py
+++ apis/unified_api/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 # Riktig modulnavn: unified_api (underscore)
 from apis.unified_api.routers.rag import router as rag_router
-from apis.unified_api.routers.ingest import router as ingest_router
+# Ingest er deaktivert i Lag 1a
+# from apis.unified_api.routers.ingest import router as ingest_router
 from apis.unified_api.routers.neo4j import router as neo4j_router
 from apis.unified_api.routers.graph_rag import router as graph_rag_router
 from apis.unified_api.routers.embed import router as embed_router
@@ -15,7 +16,7 @@ def health():
 
 # inkluder routers
 app.include_router(rag_router)
-app.include_router(ingest_router)
+# app.include_router(ingest_router)  # deaktivert
 app.include_router(neo4j_router)
 app.include_router(graph_rag_router)
 app.include_router(embed_router)
